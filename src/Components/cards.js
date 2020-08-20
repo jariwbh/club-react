df483857257ad832849fd08c1a8c8af193b2dac3
krtya.admin <krtya.admin@krtyasoftwares.net>
2020-08-20 16:47:37 +0530

    update

---
+++ src/Components/cards.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import * as image from './Image'
+class cards extends Component {
+    render() {
+        return (
+            <>
+                <section class="common-block">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 text-center">
+                                <h2 class="mrb30">Success Stories</h2>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6 col-lg-4 d-flex">
+                                <div class="white-box background-transparent text-center mrb30" >
+                                    <div class="cs-img justify-content-center">
+                                        <img src={image.timesvacanza} alt="" />
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title"> Times Vacanza </h5>
+                                        <p class="card-text">Times Vacanza is part of hospitality business of Times group. They offer Holidays. </p>
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-lg-4 d-flex">
+                                <div class="white-box background-transparent text-center mrb30" >
+                                    <div class="cs-img justify-content-center">
+                                        <img src={image.yogatopics} alt="" />
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">  Yoga Topics  </h5>
+                                        <p class="card-text">Yogatopics is Mumbai based Yoga studio which offers various Yoga classes, Mind/Body  </p>
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-lg-4 d-flex">
+                                <div class="white-box background-transparent text-center mrb30" >
+                                    <div class="cs-img justify-content-center">
+                                        <img src={image.yogatopics} alt="" />
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title"> Club Hozo </h5>
+                                        <p class="card-text">Club Hozo is part of hospitality business. They offer best holiday services. </p>
+                                        <a href="#" class="btn btn-primary">Read More</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </>
+        )
+    }
+}
+
+export default cards
--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react'
 import Layout from '../Components/Layout'
-import { logo, demo } from '../Components/Image'
+import { logo, demo, pic1 } from '../Components/Image'
+import Cards from '../Components/cards'
 
 export class Home extends Component {
     render() {
         return (
             <Layout>
-                <img src={demo} height='600' width='100%' alt="" />
+                <section class="banner-main">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner" role="listbox">
+                            {/* <!-- Slide One - Set the background image for this slide in the line below --> */}
+                            <div class="carousel-item active" style={{ backgroundImage: `url(${pic1})` }} >
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h2 class="display-4">First Slide</h2>
+                                    <p class="lead">This is a description for the first slide.</p>
+                                </div>
+                            </div>
+                            {/* <!-- Slide Two - Set the background image for this slide in the line below --> */}
+                            <div class="carousel-item " style={{ backgroundImage: `url(${pic1})` }} >
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h2 class="display-4">Second Slide</h2>
+                                    <p class="lead">This is a description for the second slide.</p>
+                                </div>
+                            </div>
+                            {/* <!-- Slide Three - Set the background image for this slide in the line below --> */}
+                            <div class="carousel-item " style={{ backgroundImage: `url(${pic1})` }} >
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h2 class="display-4">Third Slide</h2>
+                                    <p class="lead">This is a description for the third slide.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </section>
+                <Cards />
             </Layout>
         )
     }
